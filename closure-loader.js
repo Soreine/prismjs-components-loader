c3ae1daf69b23242b827795fdf89b9c5d48070dd
Soreine <soreine.plume@gmail.com>
2017-06-07 13:59:33 +0200

    Clean published files

--- closure-loader.js
+++
@@ -1,5 +0,0 @@
-// Wrap the module in a closure function taking Prism as argument
-module.exports = function(source) {
-    const closure = 'module.exports = function (Prism) {' + source + '}';
-    return closure;
-};
